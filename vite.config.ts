5161741d3126cf55b26531d9ee8e9f61e58d5e13
pavith vishnu <pavith.vishnu@tinkerblox.io>
2025-08-20 15:34:35 +0530

    path fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Change base to '/<repo-name>/' after pushing to GitHub if using Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/pavith-portfolio/',
+  base: '/',
 })
